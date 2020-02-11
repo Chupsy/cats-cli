242d27bbbe8aa24dd3468189f413cda9b0101251
vincent dufrasnes <vincentdufrasnes@vincent-dufrasnes>
2020-02-11 17:07:08 +0100

    changed descript of project

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,16 @@ import clear from 'clear'
 import figlet from 'figlet'
 import program from 'commander'
 import './commands/init'
+
 clear()
 console.log(
   chalk.red(figlet.textSync('CATS-CLI', { horizontalLayout: 'full' }))
 )
-program.version('0.0.1').description("An example CLI for ordering pizza's")
+program
+  .version('0.0.1')
+  .description(
+    'CLI to create and manage clean architecture projects in typescript server-side'
+  )
 
 async function main(): Promise<void> {
   await program.parseAsync(process.argv)
