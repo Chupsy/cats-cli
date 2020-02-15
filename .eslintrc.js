8bf9a33ff223a5c03d6b1e5c5bbf9bec1036530c
Matías Plaza <matiaschileno@gmail.com>
2020-02-15 12:02:44 +0100

    disable rules for es-syntax as code is transpiled

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'jest', 'node'],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:jest/recommended',
-        'plugin:node/recommended',
-        'prettier/@typescript-eslint',
-    ],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest', 'node'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:node/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  rules: {
+    'node/no-unsupported-features/es-syntax': 0,
+  },
 };
